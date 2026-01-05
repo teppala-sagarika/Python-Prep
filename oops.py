6b316a3d6be2bc060ba3b1d2c1234c28849a2522
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-05 22:09:58 +0530

    instance and class attributes

--- oops.py
+++ oops.py
@@ -1,8 +1,13 @@
 #class
 class Student:
+    #class attributes
+    clg="VIIT"
+    PI=3.14
     def __init__(self,name,cgpa): #constructor - for initializing object
+        #instance attributes
         self.name=name
         self.cgpa=cgpa
+        self.PI=3.1
 
 
 #objects
@@ -10,3 +15,10 @@ stu1=Student("Sagarika",9.3)
 stu2=Student("Moukhika",8.2)
 print("Names=",stu1.name," ",stu2.name)
 print("CGPAs=",stu1.cgpa," ",stu2.cgpa)
+
+#class and instance attributes value check
+print(Student.clg)
+print(stu1.clg)
+print(stu1.name)
+print(stu1.PI)
+print(Student.PI)
