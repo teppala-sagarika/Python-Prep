850ee5ff5078f744eb30f033491a8ba7bf84cd37
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-05 21:54:02 +0530

    Accessing object attributes

--- oops.py
+++ oops.py
@@ -1,8 +1,12 @@
 #class
 class Student:
-    def __init__(self): #constructor - for initializing object
-        print("constructor was called")
+    def __init__(self,name,cgpa): #constructor - for initializing object
+        self.name=name
+        self.cgpa=cgpa
+
 
 #objects
-stu1=Student()
-stu2=Student()
+stu1=Student("Sagarika",9.3)
+stu2=Student("Moukhika",8.2)
+print("Names=",stu1.name," ",stu2.name)
+print("CGPAs=",stu1.cgpa," ",stu2.cgpa)
