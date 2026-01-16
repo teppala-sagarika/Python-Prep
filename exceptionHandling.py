815c7bf8cac768fc4ddab8d19920acd467d7ce18
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-16 23:05:04 +0530

    Exception Handling

---
+++ exceptionHandling.py
@@ -0,0 +1,17 @@
+#EXCEPTION HANDLING
+#try, except , fnally , else
+
+try:
+    x=int(input("enter a divisor="))
+    ans=10/x
+
+except ZeroDivisionError: #divison by 0 
+    print("Division by 0 is not allowed")
+
+#u can also handle multiple exceptions for a try block
+except ValueError: #string instead of int
+    print("Invalid input")
+
+else:
+    print(f"ans = {ans}")
+
