4493b695fa4d1db500718ce3e4f3c9e9d366f28b
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-16 15:46:52 +0530

    x mode

--- modes.py
+++ modes.py
@@ -5,9 +5,16 @@
 
 # f.close()
 
-#APPEND MODE
-f=open("sample.txt","a") #file object
+# #APPEND MODE
+# f=open("sample.txt","a") #file object
 
-f.write("Text being appended to the file.")
+# f.write("Text being appended to the file.")
+
+# f.close()
+
+# CREATE A FILE 
+f=open("sample2.txt","x") #file object
+
+f.write("Some random text")
 
 f.close()
