fdc89bef5f799f9c8cb894552d571ea16732443d
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-16 15:40:39 +0530

    Append mode

--- modes.py
+++ modes.py
@@ -1,5 +1,13 @@
-f=open("sample.txt") #default is read mode
+#READ MODE
+# f=open("sample.txt") #default is read mode
 
-print(f.read())
+# print(f.read())
+
+# f.close()
+
+#APPEND MODE
+f=open("sample.txt","a") #file object
+
+f.write("Text being appended to the file.")
 
 f.close()
